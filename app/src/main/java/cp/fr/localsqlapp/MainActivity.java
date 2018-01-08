b5de5ec98bc71c0b4ddc720f169c42cf3794badb
Cyrille Paillard <cyrille.paillard@free.fr>
2018-01-08 16:54:33 +0100

    creation de la méthode pour lire la base de données

--- app/src/main/java/cp/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/cp/fr/localsqlapp/MainActivity.java
@@ -1,9 +1,17 @@
 package cp.fr.localsqlapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.cp.database.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +26,32 @@ public class MainActivity extends AppCompatActivity {
             Intent FormIntent = new Intent(this, FormActivity.class);
             startActivity(FormIntent);
 
+    }
+
+    private List<Map<String,String>> getAllcontact() {
+
+        //instencier le composant à la base de données
+        DatabaseHandler db = new DatabaseHandler(this) ;
+
+        //instancier la requete de selection
+        Cursor cursor = db.getReadableDatabase().rawQuery("SELECT * FROM contacts", null);
+
+        //instancier la liste qui recevra les données
+        List<Map<String,String>> contactList = new ArrayList<>();
+        Map<String, String> contactCols = new HashMap<>();
+
+        while(cursor.moveToNext()){
+            contactCols.put ("name",cursor.getString(0));
+            contactCols.put ("firstname",cursor.getString(1));
+            contactCols.put ("email", cursor.getString(2));
+
+            //ajout du map de la liste
+
+            contactList.add(contactCols);
+        }
+
+        //retour de curseur
+
+        return contactList;
     }
 }

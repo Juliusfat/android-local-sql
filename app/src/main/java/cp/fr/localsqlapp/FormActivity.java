174cdc3277adb61d0d61c500198b25cddcc853b8
Cyrille Paillard <cyrille.paillard@free.fr>
2018-01-08 10:36:03 +0100

    Ajout d'une nouvelle activité pour ajouter une contact

---
+++ app/src/main/java/cp/fr/localsqlapp/FormActivity.java
@@ -0,0 +1,20 @@
+package cp.fr.localsqlapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+
+    public void onValid(View V){
+        Button clickButton = (Button) V;
+
+    }
+}

0ed590560a42089cddba350a8886d5de591a74ff
Cyrille Paillard <cyrille.paillard@free.fr>
2018-01-09 10:24:07 +0100

    creation de l'adaptateur

---
+++ app/src/main/java/cp/fr/localsqlapp/ContactArrayAdapter.java
@@ -0,0 +1,55 @@
+package cp.fr.localsqlapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Formation on 09/01/2018.
+ */
+
+public class ContactArrayAdapter extends ArrayAdapter {
+    // initialisation des variables
+    private Activity context;
+    private List<Map<String,String>> data;
+    private int resource;
+    private LayoutInflater inflater;
+
+
+    public ContactArrayAdapter(@NonNull List<Map<String, String>> data) {
+        super(context, resource, data);
+        // création du constructeur
+        this.data = data;
+        this.resource =  resource;
+        this.context = (Activity) context;
+        this.inflater = this.context.getLayoutInflater();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // instenciation de la vue
+        View view = this.inflater.inflate(R.layout.contact_list_view,parent,false);
+
+        Map<String,String> contactdata = this.data.get(position);
+        // liaison entre les données et la ligne
+        TextView nametextview = view .findViewById((R.id.ListtextViewName));
+        nametextview.setText(contactdata.get("name"));
+        TextView firsttextview = view .findViewById((R.id.ListtextViewFirstname));
+        firsttextview.setText(contactdata.get("fisrtname"));
+        TextView emailtextview = view .findViewById((R.id.ListtextViewEmail));
+        emailtextview.setText(contactdata.get("email"));
+
+
+        return view;
+    }
+}
--- app/src/main/java/cp/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/cp/fr/localsqlapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,10 +16,17 @@ import fr.cp.database.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView contactListView;
+    private List<Map<String,String>> contactList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactListView = findViewById(R.id.contactlistView);
+        contactList = this.getAllcontact();
+        ContactArrayAdapter contactAdapter = new ContactArrayAdapter(this.contactList);
+
     }
 
     public void onAddContact(View view) {
